76617c61fc15272be916bcf62bd75811a3648494
huqurema18 <hugo.castiblanco08@gmail.com>
2023-05-22 23:08:53 -0500

    basic table

---
+++ work_React/dynamic-table/src/App.js
@@ -0,0 +1,28 @@
+// App.js
+import React from 'react';
+
+const App = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Edad</th>
+          <th>Cédula</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Juan</td>
+          <td>Pérez</td>
+          <td>25</td>
+          <td>12345678</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
+
